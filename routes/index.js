d44f8262686d3a8efcd22489b5984e0a9850e7dc
adscript <adnanocode@gmail.com>
2019-09-21 19:27:20 +0700

    complete login/logout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const checkAuth = require('../middleware/auth').isLoggedOut;
+
+const projectsRouter = require('./projects');
+router.use('/projects', projectsRouter);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', checkAuth, (req, res, next) => {
+  res.render('index', { messages : req.flash('info')[0] });
+});
+
+router.post('/validate', (req, res, next) => {
+  const {email, password} = req.body;
+  console.log(req.body);
+  
+  if(email == 'admin@adnan.com' && password == '1234'){
+    req.session.user = {email};
+    res.redirect('/projects');
+  }
+  else{
+    req.flash('info', 'Wrong email or password');
+    res.redirect('/');
+  }
+});
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
