a5678e01f8a8622dd748dbcce0979df7b4c5294d
adscript <adnanocode@gmail.com>
2019-09-26 20:27:58 +0700

    modified spaces

--- model/profile.js
+++ model/profile.js
@@ -1,7 +1,6 @@
 module.exports = class Profile {
-    static updateProfile(pool, Query = [], arrValue = [], counter){
-        let sqlUpdated = `UPDATE users SET ${Query.join(', ')} WHERE userid = $${counter}`;
-        console.log(sqlUpdated);
-        return pool.query(sqlUpdated, arrValue);
+    static updateProfile(pool, filterQuery = [], filterValue = [], counter){
+        let sqlUpdated = `UPDATE users SET ${filterQuery.join(', ')} WHERE userid = $${counter}`;
+        return pool.query(sqlUpdated, filterValue);
     }
 }
