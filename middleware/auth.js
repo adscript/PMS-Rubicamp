8e39d0a0cd6056e6fb087ec05a72b752633f349a
adscript <adnanocode@gmail.com>
2019-09-26 20:26:36 +0700

    check isadmin for logged in users

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,5 +11,12 @@ module.exports = {
             next();
         else
             res.redirect('/');
+    },
+
+    isAdmin: (req, res, next) => {
+        if(req.session.user.isadmin)
+            next();
+        else
+            res.redirect('/projects');
     }
 }
